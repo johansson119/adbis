b9aafb555782a153ef88e729d1765aba73da3815
johansson119 <johansson6950@gmail.com>
2026-04-28 18:05:53 +0200

    start opgave funktion

--- alfalaval-prototype/routes/tasks.js
+++ alfalaval-prototype/routes/tasks.js
@@ -39,4 +39,58 @@ router.get("/:id", (req, res) => {
     );
   });
 
+  // Start Task – Gem starttidspunkt i databasen
+  router.post("/:id/start", (req, res) => {
+    const id = req.params.id;
+  
+    const now = new Date().toISOString();
+  
+    db.run(
+      "UPDATE tasks SET start_time = ? WHERE id = ?",
+      [now, id],
+      function (err) {
+        if (err) return res.status(500).json(err);
+        res.json({ success: true, start_time: now });
+      }
+    );
+  });
+
+  // Afslutter opgaven ved at beregne tidsforbrug, gemme servicerapporten og opdatere status til COMPLETED
+  router.post("/:id/submit", (req, res) => {
+    const taskId = req.params.id;
+    const { problem_description, work_performed, images } = req.body;
+  
+    const endTime = new Date().toISOString();
+  
+    db.get("SELECT start_time FROM tasks WHERE id = ?", [taskId], (err, task) => {
+      if (err) return res.status(500).send("Database error");
+  
+      const start = new Date(task.start_time);
+      const end = new Date(endTime);
+      const timeSpent = Math.round((end - start) / 60000);
+  
+      db.run(
+        `
+        INSERT INTO service_reports 
+        (task_id, problem_description, work_performed, images, date_submitted, time_spent)
+        VALUES (?, ?, ?, ?, ?, ?)
+        `,
+        [taskId, problem_description, work_performed, images, endTime, timeSpent]
+      );
+  
+      db.run(
+        "UPDATE tasks SET end_time = ?, status = ? WHERE id = ?",
+        [endTime, "COMPLETED", taskId],
+        (err) => {
+          if (err) return res.status(500).send("Database error");
+  
+          res.json({
+            message: "Report submitted",
+            time_spent: timeSpent
+          });
+        }
+      );
+    });
+  });
+
 module.exports = router;
